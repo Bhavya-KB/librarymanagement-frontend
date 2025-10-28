d446761ba24fc0138514e17f14a5ba85c4b45120
Bhavya KB <bhavyabhavadas21@gmail.com>
2025-10-28 08:29:18 +0530

    projectbase created

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+
+
+function App() {
+  
+
+  return (
+    <>
+       <Routes>
+      <Route path='/' element={<Login/>}/>
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='text-danger'>Login</div>
+  )
+}
+
+export default Login
