af177adf7b2f5b6c0be1302adf698a394663fb86
ranjima26 <ranjimar062@gmail.com>
2025-10-30 13:08:51 +0530

    admin add book created

--- src/Admin/AdminBookAdd.jsx
+++ src/Admin/AdminBookAdd.jsx
@@ -1,8 +1,110 @@
 import React from 'react'
+import { Box, TextField, Button, Typography, Rating } from '@mui/material';
+
 
 function AdminBookAdd() {
   return (
-    <div>AdminBookAdd</div>
+    <>
+    <Box sx={{ backgroundImage: 'url(https://www.shutterstock.com/image-photo/book-open-pages-close-up-600nw-2562942291.jpg)', width: '100%', height: '900px', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative' }}>
+
+
+
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'rgba(255, 255, 255, 0.85)',
+            padding: 4,
+            borderRadius: 2,
+            width: '50%',
+            maxWidth: '100%',
+            boxShadow: 5,
+
+          }}
+        >
+          <Typography variant="h5" gutterBottom align='center'>
+            Let's Add a New Read!📖
+          </Typography>
+
+          <Box component="form">
+            <TextField
+              label="Book Name"
+              name="name"
+              fullWidth
+              required
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Image URL"
+              name="image"
+              fullWidth
+              required
+              sx={{ mb: 2 }}
+              placeholder="https://www.shutterstock.com/image-photo/book-stack-opened-on-desk-260nw-2318012963.jpg"
+            />
+            <TextField
+              label="Author"
+              name="name"
+              fullWidth
+              required
+              sx={{ mb: 2 }}
+            />
+            <Box sx={{ mb: 2 }}>
+              <Typography>
+                Rating :
+              </Typography>
+              <Rating
+                name="rating"
+
+              />
+
+
+            </Box>
+
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: '#f43636ff',
+                  color: '#ffffff',
+                  '&:hover': {
+                    backgroundColor: '#d12f2f', 
+                  },
+                }}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  backgroundColor: '#983508ff',
+                  color: '#ffffff',
+                  '&:hover': {
+                    backgroundColor: '#7a2a06', 
+                  },
+                }}
+              >
+                Add Book
+              </Button>
+
+
+            </Box>
+
+
+
+
+
+          </Box>
+        </Box>
+
+      </Box>
+    
+    </>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import AdminHome from './Admin/AdminHome'
+import AdminBookAdd from './Admin/AdminBookAdd'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/adminhome' element={<AdminHome/>}/>
+      <Route path='/adminaddbook' element={<AdminBookAdd/>}/>
 
     </Routes>
     </>
