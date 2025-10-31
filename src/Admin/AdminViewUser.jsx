4212b2aae0ebef4f3bd3479b51e44e6d56f29a33
ranjima26 <ranjimar062@gmail.com>
2025-10-31 10:03:23 +0530

    edit modal

--- src/Admin/AdminViewUser.jsx
+++ src/Admin/AdminViewUser.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Button, Card, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardContent, Typography,Box,TextField } from '@mui/material';
+import Modal from '@mui/material/Modal';
+
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 function AdminViewUser() {
+
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <>
             <div style={{ backgroundColor: 'rgba(242, 227, 166, 0.8)', minHeight: '100vh', padding: '40px 0' }}>
+                <h1 className='font-bold  text-center' >
+                                        User Details
+                                    </h1>
                 <div className="container">
                     <div className="row ">
                         <div className="col-md-4 mb-4">
@@ -19,14 +43,7 @@ function AdminViewUser() {
                                 }}
                             >
                                 <CardContent>
-                                    <Typography
-                                        variant="h6"
-                                        gutterBottom
-                                        align="center"
-                                        fontWeight="bold"
-                                    >
-                                        User Details
-                                    </Typography>
+                                    
 
                                     <table style={{ width: '100%', borderCollapse: 'collapse' }}>
                                         <tbody>
@@ -59,6 +76,7 @@ function AdminViewUser() {
                                                 <td colSpan="2" style={{ textAlign: 'center', paddingTop: '10px' }}>
                                                     <div className="d-flex justify-content-center gap-3">
                                                         <Button
+                                                            onClick={handleOpen}
                                                             variant="contained"
                                                             sx={{
                                                                 backgroundColor: '#983508',
@@ -93,6 +111,117 @@ function AdminViewUser() {
 
                         </div>
                     </div>
+                </div>
+
+                <div>
+                    {/* <Button onClick={handleOpen}>Open modal</Button> */}
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+
+
+                        <Box
+                            sx={{
+                                backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                                padding: 4,
+                                borderRadius: 3,
+                                width: { xs: '90%', sm: '70%', md: '50%' },
+                                boxShadow: 6,
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                        >
+                            <Typography
+                                variant="h5"
+                                gutterBottom
+                                align="center"
+                                fontWeight="bold"
+                                sx={{ color: '#4a2f04' }}
+                            >
+                                Update User Details
+                            </Typography>
+
+                            <Box component="form" noValidate autoComplete="off">
+                                <TextField
+                                    label="User ID"
+                                    name="userId"
+                                    type="number"
+                                    fullWidth
+                                    required
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    label="User Name"
+                                    name="userName"
+                                    fullWidth
+                                    required
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    label="Book Name"
+                                    name="bookName"
+                                    fullWidth
+                                    required
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    label="Book Number"
+                                    name="bookNumber"
+                                    fullWidth
+                                    required
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    label="Taken Date"
+                                    name="takenDate"
+                                    type="date"
+                                    fullWidth
+                                    InputLabelProps={{ shrink: true }}
+                                    required
+
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    label="Return Date"
+                                    name="returnDate"
+                                    type="date"
+                                    InputLabelProps={{ shrink: true }}
+                                    fullWidth
+                                    required
+
+                                    sx={{ mb: 2 }}
+                                />
+
+                                <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+                                    <Button
+                                        variant="contained"
+                                        sx={{
+                                            backgroundColor: '#f43636',
+                                            '&:hover': { backgroundColor: '#d12f2f' },
+                                        }}
+                                    >
+                                        Cancel
+                                    </Button>
+
+                                    <Button
+                                        type="submit"
+                                        variant="contained"
+                                        sx={{
+                                            backgroundColor: '#983508',
+                                            '&:hover': { backgroundColor: '#7a2a06' },
+                                        }}
+                                    >
+                                        Update
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Modal>
                 </div>
             </div>
 
